7fa31c1139dde2136e8b3d2f29e681e978e89b0f
LJKS <Leon-Schmid@gmx.de>
2018-01-02 15:38:52 +0100

    was da los

--- MethodsMasters/mdp.py
+++ MethodsMasters/mdp.py
@@ -3,6 +3,6 @@ from gridWorld import GridWorld
 
 class mdp:
     def __init__ (self, path):
-        self.world = gridWorld.GridWorld(path)
+        self.world = GridWorld(path)
 
 hallo = mdp('3by4.grid')
