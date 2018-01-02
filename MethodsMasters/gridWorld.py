c045270a5f8c2d0aeb6d611bf71fdf31ca1b1314
Britta Walkenhorst <bwalkenhorst@uos.de>
2018-01-02 15:18:49 +0100

    removed camel cases

--- MethodsMasters/gridWorld.py
+++ MethodsMasters/gridWorld.py
@@ -1,26 +1,26 @@
-
 import numpy as np
+
 
 class GridWorld:
 
     def __init__(self, path):
-        self.directions = {'up':[-1,0], 'down':[1,0], 'left': [0,-1], 'right':[0,1]}
+        self.directions = {'up': [-1, 0], 'down': [1, 0], 'left': [0, -1], 'right': [0, 1]}
         self.values = {'F': -0.04, 'O': 0, 'P': -1, 'E': 1}
-        self.map = self.readFile(path)
-        self.buildWeightMap()
+        self.map = self.read_file(path)
+        self.build_weight_map()
 
-    def readFile(self, path):
+    def read_file(self, path):
         tmp = []
         with open(path) as f:
             for line in f.readlines():
                 tmp.append(line.split())
         return np.array(tmp)
 
-    def buildWeightMap(self):
+    def build_weight_map(self):
         weight_map = np.zeros(np.shape(self.map))
         for key in self.values:
             key_map = self.map == key
-            key_map = key_map.astype(int)*self.values[key]
+            key_map = key_map.astype(int) * self.values[key]
             weight_map += key_map
         print(weight_map)
         return weight_map
@@ -28,24 +28,24 @@ class GridWorld:
     def step(self, state, action):
         goal = state + self.directions[action]
         relative_pos = self.map.shape - goal
-        #special treatment for endstate
+        # special treatment for endstate
         if self.map[state] == 'P' or map[state] == 'E':
             return 'END'
-        #check left and upper out of bounds
-        elif goal.min()<0:
+        # check left and upper out of bounds
+        elif goal.min() < 0:
             return state
-        #check lower and right out of bounds
+        # check lower and right out of bounds
         elif relative_pos.min() < 0:
             return state
-        #check for run against wall
+        # check for run against wall
         elif self.map[goal] == 'O':
             return state
-        #else we just take the step
+        # else we just take the step
         else:
             return goal
 
     def get_weightmap(self):
         return self.weight_map
 
-    def getSize(self):
+    def get_size(self):
         return np.shape(self.map)
