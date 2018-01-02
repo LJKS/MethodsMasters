b9a7b254f45e59f2e8c14bbaeea34a75edb0b865
Britta Walkenhorst <bwalkenhorst@uos.de>
2018-01-02 14:33:23 +0100

    missing link

--- MethodsMasters/gridWorld.py
+++ MethodsMasters/gridWorld.py
@@ -1,10 +1,10 @@
-
 import numpy as np
+
 
 class GridWorld:
 
     def __init__(self, path):
-        self.directions = {'up':[-1,0], 'down':[1,0], 'left': [0,-1]'right':[0,1]}
+        self.directions = {'up': [-1, 0], 'down': [1, 0], 'left': [0, -1], 'right': [0, 1]}
         self.values = {'F': -0.04, 'O': 0, 'P': -1, 'E': 1}
         self.map = self.readFile(path)
         self.buildWeightMap()
@@ -20,7 +20,7 @@ class GridWorld:
         weight_map = np.zeros(np.shape(self.map))
         for key in self.values:
             key_map = self.map == key
-            key_map = key_map.astype(int)*self.values[key]
+            key_map = key_map.astype(int) * self.values[key]
             weight_map += key_map
         print(weight_map)
         return weight_map
@@ -28,19 +28,19 @@ class GridWorld:
     def step(self, state, action):
         goal = state + self.directions[action]
         relative_pos = self.map.shape - goal
-        #special treatment for endstate
+        # special treatment for endstate
         if self.map[state] == 'P' or map[state] == 'E':
             return 'END'
-        #check left and upper out of bounds
-        elif goal.min()<0:
+        # check left and upper out of bounds
+        elif goal.min() < 0:
             return state
-        #check lower and right out of bounds
+        # check lower and right out of bounds
         elif relative_pos.min() < 0:
             return state
-        #check for run against wall
+        # check for run against wall
         elif self.map[goal] == 'O':
             return state
-        #else we just take the step
+        # else we just take the step
         else:
             return goal
 
@@ -49,8 +49,6 @@ class GridWorld:
 
     def getSize(self):
         return np.shape(self.map)
-
-
 
 
 hallo = GridWorld('a')
--- main.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-with open('MethodsMasters/3by4.grid') as f:
-    lines =np.array ([i.split() for i in f.readlines()])
-
-print(lines)
