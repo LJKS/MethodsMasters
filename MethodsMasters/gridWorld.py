9785a26298b7247841a3df9f7afc74ea26fc00f5
pphilihpp <phil.fu@web.de>
2018-01-02 17:48:56 +0100

    policy evaluation

--- MethodsMasters/gridWorld.py
+++ MethodsMasters/gridWorld.py
@@ -4,7 +4,7 @@ import numpy as np
 class GridWorld:
 
     def __init__(self, path):
-        self.directions = {'up': [-1, 0], 'down': [1, 0], 'left': [0, -1], 'right': [0, 1]}
+        self.directions = {'up': [0,-1], 'down': [0, 1], 'left': [-1, 0], 'right': [1, 0]}
         self.values = {'F': -0.04, 'O': 0, 'P': -1, 'E': 1}
         self.map = self.read_file(path)
         self.build_weight_map()
--- MethodsMasters/mdp.py
+++ MethodsMasters/mdp.py
@@ -7,14 +7,37 @@ class mdp:
 
     def create_policy(self):
         #TODO
-        directions = GridWorld.directions(self.world)
+        #directions = GridWorld.directions(self.world)
+        #directions = self.world.directions
+        directions = GridWorld.get_directions(self)
+        
 
-
-    def evaluate_policy(self):
+    def evaluate_policy(self,policy, max_steps):
         #TODO
+        evaluated_policy = np.zeros(shape = policy.shape)
+        while(n < max_steps):
+            for x,y in range(self.world):
+                evaluated_policy[x,y] = calculate_step(x,y,policy) 
+            n += 1
+        return evaluated_policy
+    
+    def calculate_step(self,x,y,policy):
+        gamma = 1
+        state = np.array([x,y])
+        forward_step = GridWorld.step(state,policy[x,y])
+        if(policy[x,y] == 'u' or policy[x,y] == 'd'):
+            side_step_one = GridWorld.step(state,'l')
+            side_step_two = GridWorld.step(state,'r')
+        elif(policy[x,y] == 'l' or policy[x,y] == 'r'):
+            side_step_one = GridWorld.step(state,'u')
+            side_step_two = GridWorld.step(state,'d')
+           
+        reward = -0.04 +gamma*(0.8* self.world[forward_step]+0.1*self.world[side_step_one]+0.1*self.world[side_step_two])
+        return reward
 
     def iterate_policy(self):
         #TODO
+        s 
 
     def print_solutions(self):
         #TODO
