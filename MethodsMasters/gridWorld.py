9691d18998b5faa02e1702bf10d39fc8c73d5627
LJKS <Leon-Schmid@gmx.de>
2018-01-02 14:23:11 +0100

    Added functionality in gridWorld.py

--- MethodsMasters/gridWorld.py
+++ MethodsMasters/gridWorld.py
@@ -4,8 +4,11 @@ import numpy as np
 class GridWorld:
 
     def __init__(self, path):
-        self.map = readFile(path)
-        
+        self.directions = {'up':[-1,0], 'down':[1,0], 'left': [0,-1]'right':[0,1]}
+        self.values = {'F': -0.04, 'O': 0, 'P': -1, 'E': 1}
+        self.map = self.readFile(path)
+        self.buildWeightMap()
+
     def readFile(self, path):
         tmp = []
         with open('3by4.grid') as f:
@@ -13,13 +16,41 @@ class GridWorld:
                 tmp.append(line.split())
         return np.array(tmp)
 
-    ###readfile
-    ### get file name
-    filename = '3by4.grid'
-    file = open(filename, 'r')
-    out2 = file.readlines()
-    print(len(out2))
+    def buildWeightMap(self):
+        weight_map = np.zeros(np.shape(self.map))
+        for key in self.values:
+            key_map = self.map == key
+            key_map = key_map.astype(int)*self.values[key]
+            weight_map += key_map
+        print(weight_map)
+        return weight_map
+
+    def step(self, state, action):
+        goal = state + self.directions[action]
+        relative_pos = self.map.shape - goal
+        #special treatment for endstate
+        if self.map[state] == 'P' or map[state] == 'E':
+            return 'END'
+        #check left and upper out of bounds
+        elif goal.min()<0:
+            return state
+        #check lower and right out of bounds
+        elif relative_pos.min() < 0:
+            return state
+        #check for run against wall
+        elif self.map[goal] == 'O':
+            return state
+        #else we just take the step
+        else:
+            return goal
+
+    def get_weightmap(self):
+        return self.weight_map
+
+    def getSize(self):
+        return np.shape(self.map)
 
 
 
-    print(np.shape(grid))
+
+hallo = GridWorld('a')
