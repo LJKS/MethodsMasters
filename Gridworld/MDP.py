d29f9773d0eb4f3048975e162717bb65a55125a6
Britta Walkenhorst <bwalkenhorst@uos.de>
2018-01-17 17:38:23 +0100

    just exchanged ''' with """

--- Gridworld/MDP.py
+++ Gridworld/MDP.py
@@ -1,19 +1,19 @@
+import config
 import numpy as np
 import pandas as pd
-import config
 
 
 class MDP:
 
     def calculate_action(self, position, action):
-        '''
+        """
          calculates the cost/reward for certain action on certain position
         :param position: the current position on the grid
         :param action: the action to be performed
         :return: reward/cost for doing currently assigned action on current position
-        '''
+        """
 
-        if (action in config.DIRECTIONS):
+        if action in config.DIRECTIONS:
             # TODO find a better way to add the other directions
             forward_step = self.gridworld.step(position, action)
             if action == config.DIRECTION_NORTH or action == config.DIRECTION_SOUTH:
@@ -32,10 +32,10 @@ class MDP:
         return self.gridworld.value_grid[position]
 
     def choose_greedy_action(self, position):
-        '''
+        """
         helper function for policy improvement
         :returns most greedy action for a certain position in the grid
-        '''
+        """
         action = self.gridworld.policy_grid[position]
         if action in config.DIRECTIONS:
             max_value = self.calculate_action(position, action)
@@ -47,10 +47,11 @@ class MDP:
         return action
 
     def evaluate_policy(self):
-        '''evaluates current policy n-times
+        """
+        evaluates current policy n-times
         - n defined by evaluation steps parameter of MDP
         - sets new value grid for Gridworld
-        '''
+        """
         self.gridworld.reset_value_grid()
         evaluated_grid = np.array(self.gridworld.value_grid)
         n = 0
@@ -63,10 +64,10 @@ class MDP:
             self.gridworld.value_grid = evaluated_grid
 
     def improve_policy(self):
-        '''
+        """
         find a greedy policy
         - assigns a  new improved policy to Gridworld
-        '''
+        """
         greedy_policy = np.array(self.gridworld.policy_grid)
         for x in range(np.shape(self.gridworld.policy_grid)[0]):
             for y in range(np.shape(self.gridworld.policy_grid)[1]):
@@ -74,11 +75,11 @@ class MDP:
         self.gridworld.policy_grid = greedy_policy
 
     def iterate_policy(self):
-        '''
+        """
         policy iteration until convergence
         - evaluate policy
         - improve policy
-        '''
+        """
         converged = False
         while not converged:
             old_policy = np.array(self.gridworld.policy_grid)
@@ -89,12 +90,12 @@ class MDP:
             print(self.calculation_string)
 
     def print_output(self):
-        '''
+        """
         prints the data from the MDP:
         - the original grid handed it
         - the optimal policy found for the grid
         - the policy values of the optimal policy
-        '''
+        """
         grid = pd.DataFrame(self.gridworld.source_grid)
         print('\n Loaded Grid: \n', grid.to_string(index=False))
         policy = pd.DataFrame(self.gridworld.policy_grid)
@@ -103,13 +104,13 @@ class MDP:
         print('\n Policy values: \n', values.to_string(index=False), '\n')
 
     def __init__(self, gridworld, evaluation_steps, gamma, move_cost):
-        '''
+        """
         Performs a policy iteration for a given gridworld and prints the outcome
 
         :param gridworld: The gridworld to work with
         :param evaluation_steps: the maximum amount of policy evaluation steps to be performed
         :param gamma: the gamma value for the evaluation (future reward)
-        '''
+        """
         # calculation string printed as user feedback
         self.calculation_string = 'Calculating '
         self.gridworld = gridworld
--- Gridworld/main.py
+++ Gridworld/main.py
@@ -6,10 +6,10 @@ from MDP import MDP
 
 
 def get_file_path():
-    '''
+    """
     ask user to enter path to grid file
     :return: the path entered
-    '''
+    """
     path = ''
     exists = False
     while not exists:
@@ -22,10 +22,10 @@ def get_file_path():
 
 
 def get_gamma():
-    '''
+    """
     ask user for gamma value for policy evaluation
     :return: entered gamma value
-    '''
+    """
     gamma = 2
     while gamma < 0 or gamma > 1:
         try:
@@ -36,10 +36,10 @@ def get_gamma():
 
 
 def get_evaluation_steps():
-    '''
+    """
     ask user for max policy evaluation steps
     :return: entered amount of steps
-    '''
+    """
     steps = 0
     while steps <= 0:
         try:
@@ -50,10 +50,10 @@ def get_evaluation_steps():
 
 
 def get_move_cost():
-    '''
+    """
     ask user for move costs fore each single move
     :return: entered amount of costs
-    '''
+    """
     steps = 0
     move = False
     while not move:
@@ -66,21 +66,21 @@ def get_move_cost():
 
 
 def load_grid(url):
-    '''
+    """
     loads the grid file and turns it into numpy array
     :param url: the path to load from
     :return: a numpy array based on grid
-    '''
+    """
     with open(url) as file:
         lines = np.array([i.split() for i in file.readlines()])
     return lines
 
 
 def start_again():
-    '''
+    """
     asks the user whether to terminate the program or restart
     :return: boolean whether to restart
-    '''
+    """
     while True:
         user_input = input('Do you want to start again? (y/n)')
         # user_input = user_input.strip()
@@ -93,9 +93,9 @@ def start_again():
 
 
 def start_grid_mdp():
-    '''
+    """
     starts the program, restarts if the user wants to
-    '''
+    """
     grid = load_grid(get_file_path())
     world = GridWorld(grid)
     move_costs = get_move_cost()
